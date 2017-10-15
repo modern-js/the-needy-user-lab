3121447850e052c3476e5aed06708acd73ec2af6
Martin (Anarcroth) <mdinestorov@gmail.com>
2017-10-15 18:22:00 +0300

    martin

--- client/api.js
+++ client/api.js
@@ -3,13 +3,39 @@ if (typeof global === 'undefined') {
   global = window;
 }
 
-/* write your code here */
+get = (choice) => {
+  return global.fetch(`/${choice}`);
+};
 
+get.me = (choice) => {
+  return global.fetch(`/${choice}`);
+};
 
+get.me.a = (choice) => {
+  return global.fetch(`/${choice}`);
+};
 
-// define the api-client object to be exported
+get.me.everything = () => {
+  const { food, drink, tv } = {
+    food: global.fetch(`/food`),
+    drink: global.fetch(`/drink`),
+    tv: global.fetch(`/tv`),
+  };
+
+  return Promise.all([ food, drink, tv ]);
+};
+
+const go = {
+  get
+};
+
+go.get.me = get.me;
+go.get.me.a = get.me.a;
+go.get.me.everything = get.me.everything;
+
 const api = {
-  
+  get,
+  go,
 };
 
 // making the code work both in the browser and node.js
--- server/api.js
+++ server/api.js
@@ -1,5 +1,35 @@
 const api = require('express')();
 
-/* your code here */
+let reqestNum = 0;
+
+api.get('/food', (req, res) => {
+    if ( reqestNum > 9 ) {
+        res.send({ error: 4 });
+
+    } else {
+        reqestNum += 1;
+        res.send({ food: 1 });
+    }
+});
+
+api.get('/drink', (req, res) => {
+    if ( reqestNum > 9 ) {
+        res.send({ error: 4 });
+
+    } else {
+        reqestNum += 1;
+        res.send({ drink: 2 });
+    }
+});
+
+api.get('/tv', (req, res) => {
+    if ( reqestNum > 9 ) {
+        res.send({ error: 4 });
+
+    } else {
+        reqestNum += 1;
+        res.send({ tv: 3 });
+    }
+});
 
 module.exports = api;
